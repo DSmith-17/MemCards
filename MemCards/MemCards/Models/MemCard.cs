ebe283fbaab70398068df67c39511d3ec549a37f
Dylan Smith <dsmith@ntiertech.com>
2018-08-21 01:09:10 -0400

    init

---
+++ MemCards/MemCards/Models/MemCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemCards.Models
+{
+    class MemCard
+    {
+
+        public string Name { get; set; }
+
+        public string FriendlyName { get; set; }
+
+        public string Desc { get; set; }
+
+   
+
+
+
+
+
+
+    }
+}
